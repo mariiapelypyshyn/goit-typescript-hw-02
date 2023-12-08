5f01bdf8cbdfdf43fe108adf376b0477e9aae0a8
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-12-08 15:51:44 +0200

    Homework 2

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,23 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DaysOfWeek {
+  Monday = 'Понеділок',
+  Tuesday = 'Вівторок',
+  Wednesday = 'Середа',
+  Thursday = 'Четвер',
+  Friday = 'П\'ятниця',
+  Saturday = 'Субота',
+  Sunday = 'Неділя'
+}
+
+function isWeekend(day: DaysOfWeek): boolean {
+  switch (day) {
+    case DaysOfWeek.Saturday:
+    case DaysOfWeek.Sunday:
+      return true; 
+    default:
+      return false; 
+  }
+}
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -14,7 +14,7 @@ type User = {
   password: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser(initialValues: Pick<User, "email"|"password">) {
   // Оновлення користувача
 }
 
